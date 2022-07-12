7f7c0746bf3641ad3c1f0ce5d19e72165daba696
btotheraxxx <108935737+btotheraxxx@users.noreply.github.com>
2022-07-12 12:01:51 -0500

    Add files via upload

---
+++ FreeBTC/claimfreebtc/cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//Edit This Cookie And Setup Your OrgiOriginal Cookie //
+// looked Tutorial Video 
+
+
+$cookie ='_ga=GA1.1.1116487117.1651762944;dom3ic8zudi28v8lr6fgphwffqoz0j6c=6338d483-be57-4f80-b028-99013a650fcd%3A1%3A1;comm100_visitorguid_40101725=4474c069-8d81-4214-a6bd-65b13e6602bd;_ga_NMD50SMBG1=GS1.1.1651927989.2.1.1651928092.0;csrf_cookie_name=f8374ee1ac2a8124dfd384a0df1f411a;ci_session=c9040c7892647c970e1f66694779159a12a15b8d';
+
+
+$user ='Mozilla/5.0 (Linux; Android 10; i67) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.99 Mobile Safari/537.36';
